177ba727a1a1933241345897cd683b7755aa510f
Nivedin <nivedinp@gmail.com>
2021-07-31 00:59:17 +0530

    added new photo for home

--- src/util/homeNoticeList.js
+++ src/util/homeNoticeList.js
@@ -7,6 +7,26 @@ const homeNoticeList = [
     id: 2,
     imgSrc: "/images/notice/Room Rate 1.png",
   },
+  {
+    id: 3,
+    imgSrc: "/images/notice/IMG_7476 sky replaced 3.png",
+  },
+  {
+    id: 4,
+    imgSrc: "/images/notice/ultra sound scan 2.png",
+  },
+  {
+    id: 5,
+    imgSrc: "/images/notice/lab 2.png",
+  },
+  {
+    id: 6,
+    imgSrc: "/images/notice/labour room 2.png",
+  },
+  {
+    id: 7,
+    imgSrc: "/images/notice/operation theather 2.png",
+  },
 ];
 
 export default homeNoticeList;
