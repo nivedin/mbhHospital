27d4f2ca474015bef720342346b004ff24fe573f
nivedin <nivedinp@gmail.com>
2022-07-27 14:48:10 +0530

    chore: add new home gallery carousel image

--- src/util/homeNoticeList.js
+++ src/util/homeNoticeList.js
@@ -1,26 +1,30 @@
 const homeNoticeList = [
   {
     id: 1,
-    imgSrc: "/images/notice/mbh 1 (1).png",
+    imgSrc: "/images/notice/drShaji.jpg",
   },
   {
     id: 2,
-    imgSrc: "/images/notice/Room Rate 1.png",
+    imgSrc: "/images/notice/mbh 1 (1).png",
   },
   {
     id: 3,
-    imgSrc: "/images/notice/IMG_7476 sky replaced 3.png",
+    imgSrc: "/images/notice/Room Rate 1.png",
   },
   {
     id: 4,
-    imgSrc: "/images/notice/ultra sound scan 2.png",
+    imgSrc: "/images/notice/IMG_7476 sky replaced 3.png",
   },
   {
     id: 5,
-    imgSrc: "/images/notice/lab 2.png",
+    imgSrc: "/images/notice/ultra sound scan 2.png",
   },
   {
     id: 6,
+    imgSrc: "/images/notice/lab 2.png",
+  },
+  {
+    id: 7,
     imgSrc: "/images/notice/labour room 2.png",
   },
   // {
